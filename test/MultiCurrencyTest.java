5510ee1b6cb729d76138a3bb0113e04767ddfe9b
minsik2434 <minsik2434@gmail.com>
2025-07-04 21:50:43 +0900

    Junit 추가

---
+++ test/MultiCurrencyTest.java
@@ -0,0 +1,24 @@
+
+import org.junit.jupiter.api.Test;
+import test1.Dollar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MultiCurrencyTest {
+
+    @Test
+     void testMultiplication(){
+        Dollar five = new Dollar(5);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        five.times(3);
+        assertEquals(15, product.amount);
+    }
+
+    @Test
+    void testEquality(){
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
+    }
+}
